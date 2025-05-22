0c1071185e9b5130920fc8c0ad5a98c477db4ed5
ABENET ABEJE <qeneqene45@gmail.com>
2025-05-22 18:00:57 +0300

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,6 +1,12 @@
-export type RowID = number;
-export interface RowElement {
-  firstName: string;
-  lastName: string;
-  age?: number;
-}
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud.js';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva'
+};
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = { ...row, age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
